7d943b73ba936beb6981311286a362ae56a35590
Vanessa Schneider <vanessajwade@gmail.com>
2022-01-04 18:51:46 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+const { getNamespaceURI } = require("jsdom/lib/jsdom/browser/documentAdapter")
+
+
+function writeCards(names, celebration){
+    let messages = []
+for (let i = 0; i < names.length; i++){
+    messages.push (`Thank you, ${names[i]}, for the wonderful ${celebration} gift!`);}
+
+
+    
+    return messages;
+}
+
+
+writeCards(names, celebration);
+
+function countDown(){
+let countDown = 10;
+while (countDown >= 0) {
+  console.log(countDown--); 
+
+}}
